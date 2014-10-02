5a79646ac6f35242e20d6dc30913450523fe9e8a
alessandromarty <ale.50special@hotmail.it>
2014-10-02 18:30:19 +0200

    qweffdsaù

--- E03/analisi/Gol.py
+++ E03/analisi/Gol.py
@@ -7,15 +7,36 @@ import numpy as np
 	####  Qui vanno i dati 	####
 dataG1 = np.genfromtxt("../dati/4.1.csv", delimiter=',')
 
-f_1	= dataG1[1:,0]
-V_A1	= dataG1[1:,1]
-V_out1	= dataG1[1:,2]
+f_1	= dataG1[3:,0]
+V_A1	= dataG1[3:,1]
+V_out1	= dataG1[3:,2]
 
 dataG2 = np.genfromtxt("../dati/4.2.csv", delimiter=',')
 
-f_2	= dataG2[2:,0]#[2:7,0]
-V_A2	= dataG2[2:,1]#[2:7,1]
-V_out2	= dataG2[2:,2]#[2:7,2]
+f_2	= dataG2[3:,0]#[2:7,0]
+V_A2	= dataG2[3:,1]#[2:7,1]
+V_out2	= dataG2[3:,2]#[2:7,2]
+
+
+dataG3 = np.genfromtxt("../dati/4.3.csv", delimiter=',')
+
+f_3	= dataG3[3:,0]#[2:7,0]
+V_A3	= dataG3[3:,1]#[2:7,1]
+V_out3	= dataG3[3:,2]#[2:7,2]
+
+
+dataG4 = np.genfromtxt("../dati/4.4.csv", delimiter=',')
+
+f_4	= dataG4[3:,0]#[2:7,0]
+V_A4	= dataG4[3:,1]#[2:7,1]
+V_out4	= dataG4[3:,2]#[2:7,2]
+
+
+dataG5 = np.genfromtxt("../dati/4.5.csv", delimiter=',')
+
+f_5	= dataG5[3:,0]#[2:7,0]
+V_A5	= dataG5[3:,1]#[2:7,1]
+V_out5	= dataG5[3:,2]#[2:7,2]
 
 	####	####	####	####
 
