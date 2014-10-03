44e9ce6929b45148af7eed517c4c5194aa8ac180
DejavuITA <Bazzanella.Davide@gmail.com>
2014-10-03 16:19:56 +0200

    breve push

--- E03/analisi/sr_uad.py
+++ E03/analisi/sr_uad.py
@@ -8,12 +8,12 @@ from mpl_toolkits.axes_grid1 import host_subplot
 import mpl_toolkits.axisartist as AA
 
 #### dati ####
-data01 = np.genfromtxt("../dati/scope_10.csv", delimiter=',') ##### ← cambiare qui con i file giusti
+data01 = np.genfromtxt("../dati/nuovi/tt_0210_02.csv", delimiter=',') ##### ← cambiare qui con i file giusti
 t_01 = data01[2:,0]
 VS_01 = data01[2:,1]
 VS_02 = data01[2:,2]
 
-data02 = np.genfromtxt("../dati/scope_10.csv", delimiter=',') ##### ← cambiare qui con i file giusti
+data02 = np.genfromtxt("../dati/nuovi/tt_0210_01.csv", delimiter=',') ##### ← cambiare qui con i file giusti
 t_02 = data02[2:,0]
 VS_11 = data02[2:,1]
 VS_12 = data02[2:,2]
@@ -46,12 +46,12 @@ p25t = f1.text((0.00208+0.0002)*1E3, -5-0.2, r'$P_{25\%}$', rotation='horizontal
 p90t = f1.text((0.0163+0.0002)*1E3, 8-0.2, r'$P_{90\%}$', rotation='horizontal',
 	ha='left', va='top', fontsize=22)
 
-f1.text(0.022E3/2-0.002E3, -17.3, r'tempo [$\mu s$]', rotation='horizontal',
+f1.text(25/2-3, -17.3, r'tempo [$\mu s$]', rotation='horizontal',
 	ha='center', va='center', fontsize=15)
-f1.text(-3, 0, r'd.d.p. [$V$]', rotation='vertical',
+f1.text(-4, 0, r'd.d.p. [$V$]', rotation='vertical',
 	ha='center', va='center', fontsize=15)
 
-f1.set_xlim((-0.002E3,0.02E3))
+f1.set_xlim((-3,22))
 #f1.set_ylim((-1.6,1.6))
 
 f1.grid(True)
@@ -59,27 +59,27 @@ f1.grid(True)
 # GRAFICO 2
 f2 = host_subplot(122, axes_class=AA.Axes)
 
-A=-0.002E3
-B=0.02E3
+A=-13.5
+B=38.5
 
-out1 = f2.errorbar(x=t_02*1E6, y=VS_11, fmt='-', c='0.1', linewidth=1)
-out2 = f2.errorbar(x=t_02*1E6, y=VS_12, fmt='-', c='red', linewidth=1)
+out1 = f2.errorbar(x=t_02*1E6-500, y=VS_11, fmt='-', c='0.1', linewidth=1)
+out2 = f2.errorbar(x=t_02*1E6-500, y=VS_12, fmt='-', c='red', linewidth=1)
 
-points = f2.plot([0.00048E3, 0.00208E3, 0.0163E3], [-8, -5, 8], c='black', marker='o', ls='')
+points = f2.plot([25.6, 2.6], [-8, 8], c='black', marker='o', ls='')
 
 p10h = f2.axhline(y=-8, xmin=0, xmax=1, c='gray', ls='-.', lw=1.5)
 p90h = f2.axhline(y=8, xmin=0, xmax=1, c='gray', ls='-.', lw=1.5)
-p10v = f2.axvline(x=0.00048E3, ymin=0, ymax=1, c='gray', ls='-.', lw=1.5)
-p90v = f2.axvline(x=0.0163E3, ymin=0, ymax=1, c='gray', ls='-.', lw=1.5)
+p10v = f2.axvline(x=25.6, ymin=0, ymax=1, c='gray', ls='-.', lw=1.5)
+p90v = f2.axvline(x=2.6, ymin=0, ymax=1, c='gray', ls='-.', lw=1.5)
 
-p10t = f2.text((0.00048+0.0002)*1E3, -8-0.2, r'$P_{10\%}$', rotation='horizontal',
-	ha='left', va='top', fontsize=22)
-p90t = f2.text((0.0163+0.0002)*1E3, 8-0.2, r'$P_{90\%}$', rotation='horizontal',
-	ha='left', va='top', fontsize=22)
+p10t = f2.text(25.6+0.5, -8+0.2, r'$P_{10\%}$', rotation='horizontal',
+	ha='left', va='bottom', fontsize=22)
+p90t = f2.text(2.6+0.5, 8+0.2, r'$P_{90\%}$', rotation='horizontal',
+	ha='left', va='bottom', fontsize=22)
 
 f2.text((B-A)/2+A, -17.3, r'tempo [$\mu s$]', rotation='horizontal',
 	ha='center', va='center', fontsize=15)
-f2.text(21, 0, r'd.d.p. [$V$]', rotation='vertical',
+f2.text(41, 0, r'd.d.p. [$V$]', rotation='-90',
 	ha='center', va='center', fontsize=15)
 
 f2.set_xlim((A,B))
@@ -96,7 +96,7 @@ f2.grid(True)
 
 # questo imposta i bordi del grafico
 fig1.subplots_adjust(left=0.04, right=0.96,
-    top=0.925, bottom=0.10, hspace=0.18, wspace=0.03)
+    top=0.925, bottom=0.10, hspace=0.08, wspace=0.018)
 
 # mostra grafico
 plt.show()
