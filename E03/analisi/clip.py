ce696975a8adc90f664b7699c64038d985ce8327
DejavuITA <Bazzanella.Davide@gmail.com>
2014-10-03 18:08:25 +0200

    clip

--- E03/analisi/clip.py
+++ E03/analisi/clip.py
@@ -17,33 +17,32 @@ rcParams['font.size'] = 15
 # Creo un grafico la dimensione è in pollici
 fig1 = plt.figure(figsize=(15, 7), dpi=65)
 # Titolo del grafico
-fig1.suptitle("Co", y=0.97, fontsize=17)
+fig1.suptitle("Massima corrente pilotabile dall'opamp", y=0.97, fontsize=17)
 
 # GRAFICO
 f1 = host_subplot(111, axes_class=AA.Axes)
 
-out1 = f1.errorbar(x=t_01*1E6, y=VS_01, fmt='-', c='black', linewidth=2)
-out2 = f1.errorbar(x=t_01*1E6, y=VS_02+1.7, fmt='-', c='green', linewidth=2)
+l_zero = f1.axhline(y=0, xmin=0, xmax=1, c='0.5', ls='-', lw=2)
 
-p10h = f1.axhline(y=-8, xmin=0, xmax=1, c='gray', ls='-.', lw=1.5)
-p90h = f1.axhline(y=8, xmin=0, xmax=1, c='gray', ls='-.', lw=1.5)
-p10v = f1.axvline(x=0.0589E3, ymin=0, ymax=1, c='gray', ls='-.', lw=1.5)
-p90v = f1.axvline(x=0.03555E3, ymin=0, ymax=1, c='gray', ls='-.', lw=1.5)
+out1 = f1.errorbar(x=t_01*1E3, y=VS_01, fmt='-', c='black', linewidth=2)
+out2 = f1.errorbar(x=t_01*1E3, y=VS_02, fmt='-', c='green', linewidth=2)
 
-points = f1.plot([0.0589E3, 0.03555E3], [-8, 8], c='black', marker='o', ls='')
+l_max = f1.axhline(y=1.96, xmin=0, xmax=1, c='gray', ls='--', lw=1.5)
+l_min = f1.axhline(y=-3.36, xmin=0, xmax=1, c='gray', ls='--', lw=1.5)
 
-p10t = f1.text(0.0589E3+0.0002E3, -8+0.2, r'$P_{10\%}$', rotation='horizontal',
-	ha='left', va='bottom', fontsize=22)
-p90t = f1.text(0.03555E3+0.0004E3, 8+0.2, r'$P_{90\%}$', rotation='horizontal',
-	ha='left', va='bottom', fontsize=22)
+f1.text(-0.5, 2.05, r'$V = 1.96V$', rotation='horizontal',
+	ha='center', va='bottom', fontsize=18)
+f1.text(-1.5, -3.36+0.5, r'$V = -3.36V$', rotation='horizontal',
+	ha='center', va='center', fontsize=18)
 
-f1.text(0.11E3/2-0.01E3, -16.8, r'tempo [$\mu s$]', rotation='horizontal',
+f1.text(0, -7.75, r'tempo [$ms$]', rotation='horizontal',
 	ha='center', va='center', fontsize=15)
-f1.text(-0.0125E3, 0, r'd.d.p. [$V$]', rotation='vertical',
+f1.text(-2.85, 0, r'd.d.p. [$V$]', rotation='vertical',
 	ha='center', va='center', fontsize=15)
 
-#f1.set_xlim((-0.01E3,0.1E3))
-#f1.set_ylim((-1.6,1.6))
+f1.set_xlim((-2.7,2.7))
+f1.set_ylim((-7,7))
+#f1.yaxis.set_ticks((-6,-4,-3.36,-2,0,1.96,2,4,6))
 
 f1.grid(True)
 
