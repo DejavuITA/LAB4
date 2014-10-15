7098ac1bcfb9810e68c349a3fd691bb5056b6f12
DejavuITA <Bazzanella.Davide@gmail.com>
2014-10-15 19:05:24 +0200

    debug il bug

--- E05/analisi/radd_1.py
+++ E05/analisi/radd_1.py
@@ -30,26 +30,18 @@ g1 = f1.errorbar(x=t, #x=np.logspace(70,6E6,500),
 g2 = f1.errorbar(x=t, #x=np.logspace(70,6E6,500),
 	y=V_ou,
 	fmt='-', c='green')
-    
-f1.text(-3.1, 6.4/2-1.2, u'Tensione [$V$]', size=14, va='center', ha='center',rotation='90')
-#f1.text(0, -1.67, r'Frequenza [$Hz$]', rotation='horizontal', ha='center', va='center', fontsize=15)
-#f1.text(-11.2, 0, r'Gain [$dB$]', rotation='vertical',	ha='center', va='center', fontsize=15)
-
-#f1.text(100, 38, 'G=101x', #r'$\nu_0$',
-#	size=12, va='center', ha='center')
-#f1.text(100, 23, 'G=11x', size=12, va='center', ha='center')
 
 f1.grid(True)
-#f1.set_ylim((-1.2, 5.2))
+f1.set_ylim((-0.58, 0.58))
 #f1.set_xlim((-25,25))
 
 f1.set_xlabel(u'Tempo [$m s$]', labelpad=0, fontsize=14)
-f1.text(-32.7, 0, u'Tensione [$V$]', size=12, va='center', ha='center',rotation='90')
+f1.text(-33.7, 0, u'Tensione [$V$]', size=12, va='center', ha='center',rotation='90')
 f1.legend((g1, g2), (r'$V_{in}$', r'$V_{out}$'), 'center right', prop={'size': 13})
     
 ######
 
 # questo imposta i bordi del grafico
-fig1.subplots_adjust(left=0.06, right=0.98, top=0.93, bottom=0.1, hspace=0.085, wspace=0.05)
+fig1.subplots_adjust(left=0.08, right=0.98, top=0.93, bottom=0.1, hspace=0.085, wspace=0.05)
 # mostra grafico
 plt.show()
