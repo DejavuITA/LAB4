1bed3c470325a99f967cbc568552447de1e695aa
DejavuITA <Bazzanella.Davide@gmail.com>
2014-10-15 19:45:27 +0200

    debug il bug

--- E04/analisi/oscillatore10.py
+++ E04/analisi/oscillatore10.py
@@ -16,7 +16,7 @@ V_ou	= dataG1[2:,2]
 # Creo un grafico la dimensione è in pollici
 fig1 = plt.figure(figsize=(8, 5.5))
 # Titolo del grafico
-fig1.suptitle(u'Risposta dell'"'"'oscillatore a rilassamento con $R=10k\Omega$', y=0.97, fontsize=15)
+fig1.suptitle(u'Risposta dell'"'"'oscillatore a rilassamento con $R=10k\Omega$', y=0.98, fontsize=15)
 
 ######
 # GRAFICO 1
@@ -35,28 +35,20 @@ g1 = f1.errorbar(x=t, #x=np.logspace(70,6E6,500),
 g2 = f1.errorbar(x=t, #x=np.logspace(70,6E6,500),
 	y=V_ou,
 	fmt='-', c='green')
-    
-#f1.set_ylabel(u'Tensione [$V$]', labelpad=0, fontsize=14)
-#f1.text(0, -1.67, r'Frequenza [$Hz$]', rotation='horizontal', ha='center', va='center', fontsize=15)
-#f1.text(-11.2, 0, r'Gain [$dB$]', rotation='vertical',	ha='center', va='center', fontsize=15)
-
-#f1.text(100, 38, 'G=101x', #r'$\nu_0$',
-#	size=12, va='center', ha='center')
-#f1.text(100, 23, 'G=11x', size=12, va='center', ha='center')
 
 f1.grid(True)
 f1.set_ylim((-15, 15))
-f1.set_xlim((-3,3))
+f1.set_xlim((-2.8,2.8))
 
-f1.text(-3.2, 0, u'Tensione [$V$]', size=12, va='center', ha='center',rotation='90')
+f1.text(-3, 0, u'Tensione [$V$]', size=14, va='center', ha='center',rotation='90')
 
-f1.set_xlabel(u'Tempo [$m s$]', labelpad=0, fontsize=12)
+f1.set_xlabel(u'Tempo [$m s$]', labelpad=0, fontsize=14)
 
 f1.legend((g1, g2), (r'$V_C$', r'$V_{out}$'), 'lower left', prop={'size': 13})
     
 ######
 
 # questo imposta i bordi del grafico
-fig1.subplots_adjust(left=0.07, right=0.98, top=0.93, bottom=0.11, hspace=0.085, wspace=0.05)
+fig1.subplots_adjust(left=0.07, right=0.98, top=0.93, bottom=0.1, hspace=0.085, wspace=0.05)
 # mostra grafico
 plt.show()
